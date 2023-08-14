1483ba2ba3ea0661dfc6398a56b92ed00949af9f
desktop <badtripwithurlxve@gmail.com>
2023-08-14 19:29:30 +0300

    v1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using figure_area;
+
+Shape circle = new Circle(5);
+Shape triangle = new Triangle(4, 5);
+
+double circleArea = circle.CalculateArea();
+double triangleArea = triangle.CalculateArea();
+
+Console.WriteLine("Площадь круга: " + circleArea);
+Console.WriteLine("Площадь треугольника : " + triangleArea);
